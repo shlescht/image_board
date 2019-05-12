546ec13a9502522123af73c2366b5af3c61eb283
shlescht <shlescht@gmail.com>
2019-05-12 03:45:13 -0500

    la cagué :-(

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-echo "primer parametro es lugar del arreglo.<br>";
-echo 'segundo lugar el valor del lugar en el arreglo.<br>';
-echo 'array = [<i>"caca"</i> => <b>"ex"</b>, <i>"popo"</i> => <b>"cre"</b>, <i>"mierda"</i> => <b>"mento"]</b><br>';
-echo 'imprime:<br>';
-$array = ["caca" => "ex", "popo" => "cre", "mierda" => "mento"];
-echo $array["caca"].$array["popo"].$array["mierda"];
-$array["uno"] = "chis";
-echo "<br>".$array["uno"]."<br>";
+$nombres[0] = ["nombre" => "Jorge Luis", "apellido_paterno" => "Loza", "apellido_materno" => "Gonz&aacute;les", "edad"=>"28 :'("];
+$nombres[1] = ["nombre" => "Cristobal Ra&uacute;l", "apellido_paterno" =>"Arrazola", "apellido_materno" => "Loza", "edad"=>"16"];
+$nombres[2] = ["nombre" => "Juan Carlos", "apellido_paterno" =>"Arrazola", "apellido_materno" => "Loza", "edad"=>"21"];
 
-var_dump($array);
+var_dump($nombres[0]);
+var_dump($nombres[1]);
+var_dump($nombres[2]);
+echo "<br>";
+echo "<br>";
+for ($i=0; $i < 3; $i++) {
+  echo $nombres[$i]["nombre"] . " " . $nombres[$i]["apellido_paterno"] . " " . $nombres[$i]["apellido_materno"] . " " . $nombres[$i]["edad"] . "<br>";
+}
